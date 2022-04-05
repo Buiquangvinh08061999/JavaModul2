3d06924c9105e1c4f7014c97ad4acd046dbcedc1
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-04-05 16:12:55 +0700

    Công thức tính tiền lãi

---
+++ WEEK1/UngDungTinhTienLai/UngDungTinhTienLai.java
@@ -0,0 +1,25 @@
+package UngDungTinhTienLai;
+
+import java.util.Scanner;
+
+public class UngDungTinhTienLai {
+    public static void main(String[] args) {
+        double money ;
+        int month;
+        double laiSuat;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập số tiền cho vay: ");
+        money = sc.nextDouble();
+        System.out.println("Nhập số tháng: ");
+        month = sc.nextInt();
+        System.out.println("Nhập lãi xuất: ");
+        laiSuat = sc.nextDouble();
+
+        double tongLaiSuat = 0;
+        for(int i=0; i < month;i++){
+            tongLaiSuat += money * (laiSuat/100)/12*month;
+        }
+        System.out.println("Tỗng lãi xuất là:" + tongLaiSuat);
+    }
+}
