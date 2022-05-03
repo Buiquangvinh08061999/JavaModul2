201779693f6d99bb112e3b9ff014b71421679686
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-05-03 12:26:17 +0700

    Bài tập Arraylist và LinkedList

--- src/_10_dsa_list/exercise/use_arraylist_with_library/Main.java
+++ src/_10_dsa_list/exercise/use_arraylist_with_library/Main.java
@@ -1,4 +1,12 @@
 package _10_dsa_list.exercise.use_arraylist_with_library;
 
 public class Main {
+    public static void main(String[] args) {
+        Mylist<Integer> mylist = new Mylist<Integer>();
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        System.out.println(mylist.toString());
+    }
 }
--- src/_10_dsa_list/exercise/use_linkedlist_with_library/Main.java
+++ src/_10_dsa_list/exercise/use_linkedlist_with_library/Main.java
@@ -1,4 +1,14 @@
 package _10_dsa_list.exercise.use_linkedlist_with_library;
 
 public class Main {
+    public static void main(String[] args) {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.addFirst("Tốp");
+        myLinkedList.add(1,"XÓm");
+        myLinkedList.addLast("Vinh");
+        myLinkedList.add(0,"Quang");
+        myLinkedList.printList();
+    }
+
+
 }
