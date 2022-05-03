59dceb3a33276f5c2c6ad157aa079cb4addeb702
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-05-03 12:37:29 +0700

    Stack_Queue

--- src/_11_dsa_stack_queue/_1practice/Main.java
+++ src/_11_dsa_stack_queue/_1practice/Main.java
@@ -3,5 +3,6 @@ package _11_dsa_stack_queue._1practice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("Hehe");
     }
 }
