2ed356ac39cdace900249524f114ea603b26a7ae
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-05-08 17:32:43 +0700

    [Bài tập] Đếm số lần xuất hiện của mỗi từ trong một chuỗi sử dụng Map

---
+++ src/_11_dsa_stack_queue/_2exercise/CountStringMap.java
@@ -0,0 +1,25 @@
+package _11_dsa_stack_queue._2exercise;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CountStringMap {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Nhập vào chuỗi: ");
+        String str = scanner.nextLine().toLowerCase();
+
+        String[] arrayStr = str.split(" "); /*tách chuỗi nhập vào*/
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+        for (int i = 0 ; i < arrayStr.length;i++){
+          if(treeMap.containsKey(arrayStr[i])){
+                treeMap.put(arrayStr[i], treeMap.get(arrayStr[i] + 1));
+          }else {
+                treeMap.put(arrayStr[i], 1);
+          }
+        }
+        System.out.println(treeMap);
+    }
+}
