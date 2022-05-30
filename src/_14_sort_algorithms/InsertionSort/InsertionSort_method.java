165a5e9cca7a2c427c41470c8bd0d5b291d4bd6c
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-05-30 10:37:37 +0700

    [Bài tập] Minh hoạ thuật toán sắp xếp chèn

---
+++ src/_14_sort_algorithms/InsertionSort/InsertionSort_method.java
@@ -0,0 +1,27 @@
+package _14_sort_algorithms.InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort_method {
+    public static void main(String[] args) {
+        int [] array = {10,5,3,9,1,6,11};
+        System.out.println("Mảng ban đầu là " + Arrays.toString(array));
+
+        insertionSort(array);
+        System.out.println("Mảng sau khi sắp xếp là " + Arrays.toString(array));
+    }
+
+    public static void insertionSort(int[] array){
+        int key,pos;
+        for (int i = 1; i < array.length;i++){
+            key = array[i];
+            pos = i  ;
+
+            while (pos >0 && key < array[pos-1]){
+                array[pos] = array[pos-1];
+                pos--;
+            }
+            array[pos] = key;
+        }
+    }
+}
